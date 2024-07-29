091a1f861de4636d409cfd170f788265cb96bc3b
glebl <glebleontyev2003@gmail.com>
2024-07-29 15:42:31 +0200

    Create class Coordinates

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,11 +1,31 @@
 package src;
 
 public class Coordinates {
-    private int x=0;
-    private int y=0;
+    private int x;
+    private int y;
 
-    public Coordinates(int x, int y){
+    public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 29;
+        result = 31 * result + x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Coordinates cord = (Coordinates) o;
+        return x == cord.x && y == cord.y;
     }
 }
